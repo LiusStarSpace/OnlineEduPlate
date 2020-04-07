acf2007cc90b49dd2cb88985558e7285360ec292
CN-Robert-LIU <liujhon2019@163.com>
2020-04-07 23:47:18 +0800

    upload data files

---
+++ online_edu_python_project/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/usr/home/django/online_edu_python-django/env/bin/gunicorn'
+pythonpath = '/usr/home/django/online_edu_python-django/online_edu_python_project'
+bind = '127.0.0.1:8001'
+workers = 3
